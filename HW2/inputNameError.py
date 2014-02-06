54830115d98763c0683abf473570b05ca353fb7d
Vasim Bukhari <vbukhari86@gmail.com>
2014-02-06 13:59:04 -0500

    Added what programs descriptions

--- HW2/inputNameError.py
+++ HW2/inputNameError.py
@@ -1,4 +1,10 @@
-
-NumNames = raw_input("How many names you want to enter?: ")
+#@author: Vasim Bukhari
+#Homework 2 Advance Python 
+#This program take input a list of names in "Last Name, First Name," i.e., last name, comma, first name.
+#when/if the user types the names in the wrong order, i.e., "First Name Last Name," the error is corrected,
+# and the user is notified.
+#also keep track of the number of input mistakes.
+#When the user is done,it sort the list, and display the sorted names in "Last Name, First Name" order.
+NumNames = input(raw_input("How many names you want to enter?: "))
 
 print NumNames
--- HW2/vowels.py
+++ HW2/vowels.py
@@ -1,5 +1,8 @@
 #@author: Vasim Bukhari
 # Homework 2 Advance Python 
+# This program find  the total number of vowels, consonants, and words
+#(separated by spaces) in a text sentence. Ignore special cases for vowels and
+#consonants such as "h," "y," "qu," etc.
 vowels = {'a', 'e', 'i', 'o', 'u'}
 consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k','l','m','n','p','q','r','s','t','v','w','x','z','y'}
 
