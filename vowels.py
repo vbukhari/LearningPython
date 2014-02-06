72b27f5d02f4360785b909ea64e56610630fa957
Vasim Bukhari <vbukhari86@gmail.com>
2014-02-06 13:21:35 -0500

    done with firs problem
    calculates numbers of vowels, consonants and worlds in text

--- vowels.py
+++ vowels.py
@@ -3,5 +3,13 @@
 vowels = {'a', 'e', 'i', 'o', 'u'}
 consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k','l','m','n','p','q','r','s','t','v','w','x','z','y'}
 
-word = raw_input("Please Enter Text Here: ");
-word.to 
+text = raw_input("Please Enter Text Here: ").lower();
+NumVowels = NumConsonants = NumWorlds = 0 
+
+for x in vowels: NumVowels = NumVowels + text.count(x)
+print "Number of Vowels in Text: %d" % NumVowels
+
+for x in consonants: NumConsonants = NumConsonants + text.count(x)
+print "Number of Consonants in Text: %d" % NumConsonants
+
+print "Number of Total Worlds in Text: %d" % len(text.split(" "))
