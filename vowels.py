48d7e4c47f28a8c7c30230fcbff1c0b4ea08ba37
Vasim Bukhari <vbukhari86@gmail.com>
2014-02-06 12:51:13 -0500

    HW2 - Text Processing: Vowels, Consonants

---
+++ vowels.py
@@ -0,0 +1,7 @@
+#@author: Vasim Bukhari
+# Homework 2 Advance Python 
+vowels = {'a', 'e', 'i', 'o', 'u'}
+consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k','l','m','n','p','q','r','s','t','v','w','x','z','y'}
+
+word = raw_input("Please Enter Text Here: ");
+word.to 
